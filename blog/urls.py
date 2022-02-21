d13545faf721bbb1c077a7de91adbf2f1695ac48
Daniel Coker <danielcoker28@outlook.com>
2022-02-21 03:17:05 +0100

    Add get and update post endpoint

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 
-from .views import create_post
+from .views import create_post, get_post, update_post
 
 urlpatterns = [
+    path('posts/<int:pk>/update', update_post, name='posts-update'),
+    path('posts/<int:pk>', get_post, name='posts-get'),
     path('posts/', create_post, name='posts-create'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -7,6 +7,17 @@ from .models import Post, Tag
 from .serializers import PostSerializer
 
 
+def _get_post(pk=None):
+    """
+    Fetch a post by its pk and raise a not found error
+    if it is not found.
+    """
+    try:
+        return Post.objects.get(id=pk)
+    except Post.DoesNotExist:
+        raise NotFound()
+
+
 @api_view(['POST'])
 def create_post(request):
     serializer = PostSerializer(data=request.data)
@@ -22,3 +33,30 @@ def create_post(request):
             raise NotFound()
 
     return Response(data=serializer.data, status=status.HTTP_201_CREATED)
+
+
+@api_view(['GET'])
+def get_post(request, pk=None):
+    post = _get_post(pk=pk)
+    serializer = PostSerializer(instance=post)
+
+    return Response(data=serializer.data, status=status.HTTP_200_OK)
+
+
+@api_view(['PUT'])
+def update_post(request, pk=None):
+    post = _get_post(pk=pk)
+    tags = []
+
+    for tag_id in request.data.get('tags'):
+        try:
+            tag = Tag.objects.get(id=tag_id)
+            tags.append(tag)
+        except:
+            raise NotFound()
+
+    post.tags.set(tags)
+
+    serializer = PostSerializer(instance=post)
+
+    return Response(data=serializer.data, status=status.HTTP_200_OK)
